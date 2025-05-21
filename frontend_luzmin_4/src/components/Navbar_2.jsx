3d159338dcbb9375702035ab562cd5ebee8d559e
Esteban <patiesteban810@gmail.com>
2025-05-21 17:27:21 -0400

    puse mas menus al Navbar

--- frontend_luzmin_4/src/components/Navbar_2.jsx
+++ frontend_luzmin_4/src/components/Navbar_2.jsx
@@ -4,11 +4,31 @@ const Navbar_2 = ({ onBack }) => (
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
     <div className="container-fluid">
       <span className="navbar-brand">Navbar 2</span>
+
+        <div class="container-fluid">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link 1</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link 2</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link 3</a>
+                </li>
+            </ul>
+        </div>
       <button className="btn btn-secondary ms-auto" onClick={onBack}>
         Volver
       </button>
       {/* Aquí puedes agregar el contenido de tu Navbar_2 */}
     </div>
+
+    
+
+
+
+
   </nav>
 );
 
